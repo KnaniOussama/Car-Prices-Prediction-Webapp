d9e4236655464dc78c3b2e1825affe6b0fc9f782
Oknani <oknani@yellowsys.fr>
2025-04-29 23:09:04 +0100

    feat: added login system

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import {
   Legend,
 } from 'chart.js';
 import data from './data.json';
+import { accounts } from './accounts';
 
 // Register Chart.js components
 ChartJS.register(
@@ -39,6 +40,29 @@ function App() {
   });
   const [predictedPrice, setPredictedPrice] = useState(null);
   const [error, setError] = useState(null);
+
+  const [isAuth, setIsAuth] = useState(localStorage.getItem('isAuth') || false);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if(!accounts)setError("Application is not available");
+
+    for(const account of accounts)
+    {
+      if(email=== account.email && password === account.password)
+      {
+        console.log('Logging in with:', email, password);
+        setError('');
+        localStorage.setItem('isAuth',true);
+        return setIsAuth(true);
+      }
+
+    }
+    setError('Password & email dont match any account');
+  };
 
   // Filter valid data (years between 1980 and 2023)
   const validData = data.filter(item => item.year >= 1980 && item.year <= 2023);
@@ -146,14 +170,13 @@ function App() {
       },
     ],
   };
-
+  if(isAuth)
   return (
     <div className="min-h-screen bg-gray-100 p-4">
       <div className="max-w-7xl mx-auto">
         <h1 className="text-3xl font-bold text-center mb-8 text-gray-800">
           Car Price Predictor
         </h1>
-
         {/* Form */}
         <div className="bg-white p-6 rounded-lg shadow-md mb-8">
           <form onSubmit={handleSubmit} className="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -327,6 +350,46 @@ function App() {
             />
           </div>
         </div>}
+      </div>
+    </div>
+  );
+  else
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded-2xl shadow-lg w-full max-w-md">
+        <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">Login to Your Account</h2>
+        {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
+        <form onSubmit={handleLogin} className="space-y-4">
+          <div>
+            <label className="block text-gray-700 mb-1">Email</label>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={(e)=>{setEmail(e.target.value)}}
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+              placeholder="you@example.com"
+            />
+          </div>
+          <div>
+            <label className="block text-gray-700 mb-1">Password</label>
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={(e)=>{setPassword(e.target.value)}}
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+              placeholder="••••••••"
+            />
+          </div>
+          <button
+            onClick={handleLogin}
+            type="submit"
+            className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition duration-300"
+          >
+            Login
+          </button>
+        </form>
       </div>
     </div>
   );
---
+++ src/accounts.js
@@ -0,0 +1,7 @@
+export const accounts =
+    [
+        { email: "admin@gmail.com", password: "admin" },
+        { email: "ahlem@gmail.com", password: "ahlem" },
+        { email: "khaled@gmail.com", password: "khaled" },
+        { email: "oussama@gmail.com", password: "oussama" }
+    ]
